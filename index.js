a5327fce7de1e7db929651be2253b724b6016570
Revath S Kumar <rsk@revathskumar.com>
2019-05-23 00:08:25 +0530

    send warning with details

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 "use strict";
 const exec = require("child_process").exec;
 
-const formatOutdatedPackages = (outdatedPackages = []) => {};
+const formatOutdatedPackages = (outdatedPackages = {}, packageNames = []) => {
+  const headers = [
+    "| Package | Current | Wanted | latest |",
+    "|---------|---------|--------|--------|"
+  ];
+  const content = packageNames.map(packageName => {
+    const { current, wanted, latest } = outdatedPackages[packageName];
+    return `| ${packageName} | ${current} | ${wanted} | ${latest} |`;
+  });
+  return headers.concat(content).join("\n");
+};
 
 const execP = outdatedCommand => {
   return new Promise((resolve, reject) => {
@@ -21,8 +31,25 @@ export default async function npmOutdated(options = {}) {
 
   try {
     const outdatedPackages = await execP(outdatedCommand);
-    if (outdatedPackages.length) {
-      warn(formatOutdatedPackages(outdatedPackages));
+    const packageNames = Object.keys(outdatedPackages);
+    if (packageNames.length) {
+      const packagesTable = formatOutdatedPackages(
+        outdatedPackages,
+        packageNames
+      );
+
+      warn(`You have ${packageNames.length} outdated packages`);
+      markdown(`
+
+<details>
+    <summary>Outdated Packages</summary>
+
+${packagesTable}
+
+</details>
+
+
+    `);
     }
   } catch (err) {
     fail("npm audit plugin error: " + error.message);
